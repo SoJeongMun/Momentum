3e07b5f4e6fb68a93f4a92288008666709d427fa
문소정 <106877290+SoJeongMun@users.noreply.github.com>
2023-02-04 23:26:59 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -3,7 +3,7 @@ const input = form.querySelector('input');
 const todoList = document.querySelector('.todo-list');
 
 const TODO_KEY = 'todo';
-const todos = [];
+let todos = [];
 
 function saveTodo() {
 	localStorage.setItem(TODO_KEY, JSON.stringify(todos));
@@ -12,12 +12,15 @@ function saveTodo() {
 function deleteTodo(e) {
 	const li = e.target.parentElement;
 	li.remove();
+	todos = todos.filter(todos => todos.id !== parseInt(li.id));
+	saveTodo();
 }
 
 function paintTodo(newTodo) {
 	const li = document.createElement('li');
+	li.id = newTodo.id;
 	const span = document.createElement('span');
-	span.innerText = newTodo;
+	span.innerText = newTodo.text;
 	const button = document.createElement('button');
 	button.innerText = '❌';
 	button.addEventListener('click', deleteTodo);
@@ -30,8 +33,12 @@ function handleSubmit(e) {
 	e.preventDefault();
 	const newTodo = input.value;
 	input.value = '';
-	todos.push(newTodo);
-	paintTodo(newTodo);
+	const newTodoObj = {
+		id: Date.now(),
+		text: newTodo
+	}
+	todos.push(newTodoObj);
+	paintTodo(newTodoObj);
 	saveTodo();
 }
 
@@ -41,4 +48,6 @@ const savedTodo = localStorage.getItem(TODO_KEY);
 
 if (savedTodo && savedTodo !== null) {
 	const parsedTodo = JSON.parse(saveTodo);
+	todos = parsedTodo;
+	parsedTodo.forEach(paintTodo);
 }
